d728a50bd0c7f3e74d82cfb2c249c3cea9e7ee5f
Caleb335 <belac335@gmail.com>
2020-10-11 00:12:26 +0100

    add up desxktop and mobile view

---
+++ src/assets/js/components/About.js
@@ -0,0 +1,59 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <section id='about' className='about-them'>
+      <h1 className='about-title'>ABOUT #ENDSARS</h1>
+      <div className='about__details'>
+        <p>
+          ABOUT #ENDSARS The Special Anti-Robbery Squad was founded in 1992 by
+          former police commissioner Simeon Danladi Midenda. SARS is one of the
+          14 units in the Force Criminal Investigation and Intelligence
+          Department which was established to detain, investigate and prosecute
+          people involved in crimes like armed robbery, kidnapping and other
+          forms of crimes.
+        </p>
+        <p>
+          In 2009, after several years of operations the squad grew in number
+          and strength. Due to the surge of internet fraudsters and cultism in
+          universities, SARS operatives infiltrated Nigerian universities, made
+          several successful arrest but in the process harassed innocent youths.
+          In May 2010, Amnesty International disclosed that it would be suing
+          the Nigerian Police over human rights abuse stating that the Special
+          Anti-Robbery Squad in Borokiri, Port Harcourt arrested three bike
+          riders and detained them for over one week while being "beaten every
+          night with the butt of a gun and iron belt."[5] On 20 May 2010, a
+          Federal High Court in Enugu State, ordered the then IGP Ogbonna
+          Okechukwu Onovo to produce a Special Anti-Robbery Squad officer who
+          had gunned down a 15 years-old boy in high school. According to the
+          SARS officer, the teen was mistaken for a kidnapper.[6] On 27 July
+          2010, an extensive editorial report was published by Sahara Reporters
+          on how SARS among other police unit profit 9.35Billion Naira
+          ($60million) from roadblocks and extortion within 18 months.{" "}
+        </p>
+        <p>
+          On 3 June 2011, the Nigeria Police Forced discovered an attempt by a
+          SARS operative Musa Agbu to bomb the force headquarters because the
+          IGP Hafiz Ringim scuttled his ambition.
+        </p>
+        <p>
+          Following several reports of human rights violation by members of the
+          public to the office of the Inspector General of Police, on 7 August
+          2015, the then IGP Solomon Arase announced that it would be splitting
+          the SARS unit into two units, operational unit and the investigation
+          unit to curtail case of human rights violation. In September 2016,
+          Pulse.ng compiled a report on Nigeria Police brutality with the
+          heading "Meet SARS, the Police Unit with license to kill". The report
+          highlighted the brutality and ignorance of the rules of engagement in
+          the Special Anti-Robbery Squad. In December 2016, Segun Awosanya
+          started an online advocacy campaign to end SARS brutality in the
+          country. Awonsanya started the online campaign with an hashtag,
+          #EndSARS. The public responded well to the hashtag with people all
+          over Nigeria posting their SARS experience.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
--- src/assets/js/pages/Index.jsx
+++ src/assets/js/pages/Index.jsx
@@ -2,25 +2,27 @@ import React from "react"
 import Navbar from "../components/Navbar"
 import Search from "../components/Search"
 import Button from "../components/Buttons"
+import About from "../components/About"
 
 const Index = () => {
   return (
     <div className='app__base'>
       <Navbar />
-      <section id="home">
-      <Search className='form__control search' placeholder='Search' />
-      <section className='call-to-action' id='cta'>
-        <h1>#ENDSARS</h1>
-        <div className='details'>
-          <p>
-            Nigerian Youth are mobilizing and protesting against the rampant
-            brutality and harassment of the Special Anti-Robbery Squad in
-            Nigeria. Here’s how you can help.
-          </p>
-          <p>Donate. Share. Tweet.</p>
-          <Button className='btn btn-primary'>Donate</Button>
-        </div>
-      </section>
+      <section id='home'>
+        <Search className='form__control search' placeholder='Search' />
+        <section className='call-to-action' id='cta'>
+          <h1>#ENDSARS</h1>
+          <div className='details'>
+            <p>
+              Nigerian Youth are mobilizing and protesting against the rampant
+              brutality and harassment of the Special Anti-Robbery Squad in
+              Nigeria. Here’s how you can help.
+            </p>
+            <p>Donate. Share. Tweet.</p>
+            <Button className='btn btn-primary'>Donate</Button>
+          </div>
+        </section>
+        <About />
       </section>
     </div>
   )
